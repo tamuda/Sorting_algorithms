c77884b9f42180418594d542b3635895f16dfc39
Gem Haque <gem.haque12@gmail.com>
2023-04-09 18:38:04 -0400

    format and time added (inefficiently)

--- lab6/Sorting.java
+++ lab6/Sorting.java
@@ -38,7 +38,7 @@ public class Sorting {
      * @param args the command-line arguments
      */
     public static void main(String[] args)  { 
-        In in = new In(args[0]);
+        In in = new In(args[2]);
         
 		  // Storing file input in an array
         int[] a = in.readAllInts();
@@ -79,69 +79,195 @@ public class Sorting {
         //  * 5 = Quicksort
         //  Perform sorting on a,b,c,d. Pring runtime for each case along with timestamp and record those. 
         // For runtime and priting, you can use the same code from Lab 4 as follows:
-        int algorithm= Integer.parseInt(args[1]);
+
+        // TODO: For each array, a, b, c, d:
+        Stopwatch timer = new Stopwatch();
+
+        double timeA = 0;
+        double timeB = 0;
+        double timeC = 0;
+        double timeD = 0;
+
+        int algorithm= Integer.parseInt(args[3]);
+        String name = "";
+        // TODO: Perform Sorting and store the result in an  array
         //Switch statement to select the sorting algorithm
         switch (algorithm){
             case 0:
+                name = "default";
+
+                //sorting array a w/ time using the default
+                Stopwatch timerA = new Stopwatch();
                 Arrays.sort(a);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array b w/ time using the default
+                Stopwatch timerB = new Stopwatch();
                 Arrays.sort(b);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array c w/ time using the default
+                Stopwatch timerC = new Stopwatch();
                 Arrays.sort(c);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array d w/ time using the default
+                Stopwatch timerD = new Stopwatch();
                 Arrays.sort(d);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             case 1:
+                name = "bubble";
+
+                //sorting array a w/ time using bubble sort
+                timerA = new Stopwatch();
                 bubble_sort(a);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array b w/ time using bubble sort
+                timerB = new Stopwatch();
                 bubble_sort(b);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array c w/ time using bubble sort
+                timerC = new Stopwatch();
                 bubble_sort(c);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array d w/ time using bubble sort
+                timerD = new Stopwatch();
                 bubble_sort(d);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             case 2:
+                name = "selection";
+
+                //sorting array a w/ time using selection sort
+                timerA = new Stopwatch();
                 selection_sort(a);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array b w/ time using selection sort
+                timerB = new Stopwatch();
                 selection_sort(b);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array c w/ time using selection sort
+                timerC = new Stopwatch();
                 selection_sort(c);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array d w/ time using selection sort
+                timerD = new Stopwatch();
                 selection_sort(d);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             case 3:
+                name = "insertion";
+
+                //sorting array a w/ time using insertion sort
+                timerA = new Stopwatch();
                 insertion_sort(a);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array b w/ time using insertion sort
+                timerB = new Stopwatch();
                 insertion_sort(b);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array c w/ time using insertion sort
+                timerC = new Stopwatch();
                 insertion_sort(c);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array d w/ time using insertion sort
+                timerD = new Stopwatch();
                 insertion_sort(d);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             case 4:
+                name = "merge";
                 Sorting ob = new Sorting();
+
+                //sorting array a w/ time using merge sort
+                timerA = new Stopwatch();
                 ob.sort(a, 0, a.length - 1);
-                ob.sort(b, 0, b.length - 1);
-                ob.sort(c, 0, c.length - 1);
-                ob.sort(d, 0, d.length - 1);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array a w/ time using merge sort
+                timerB = new Stopwatch();
+                ob.sort(b, 0, a.length - 1);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array a w/ time using merge sort
+                timerC = new Stopwatch();
+                ob.sort(c, 0, a.length - 1);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array a w/ time using merge sort
+                timerD = new Stopwatch();
+                ob.sort(d, 0, a.length - 1);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             case 5:
-                quick_sort(a,0,a.length-1);
-                quick_sort(b,0,b.length-1);
-                quick_sort(c,0,c.length-1);
-                quick_sort(d,0,d.length-1);
+                name = "quick";
+
+                //sorting array a w/ time using quick sort
+                timerA = new Stopwatch();
+                quick_sort(a, 0, a.length - 1);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array a w/ time using quick sort
+                timerB = new Stopwatch();
+                quick_sort(b, 0, a.length - 1);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array a w/ time using quick sort
+                timerC = new Stopwatch();
+                quick_sort(c, 0, a.length - 1);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array a w/ time using quick sort
+                timerD = new Stopwatch();
+                quick_sort(d, 0, a.length - 1);
+                timeD = timerD.elapsedTimeMillis();
                 break;
             default:
+                name = "default";
+
+                //sorting array a w/ time using the default
+                timerA = new Stopwatch();
                 Arrays.sort(a);
+                timeA = timerA.elapsedTimeMillis();
+
+                //sorting array b w/ time using the default
+                timerB = new Stopwatch();
                 Arrays.sort(b);
+                timeB = timerB.elapsedTimeMillis();
+
+                //sorting array c w/ time using the default
+                timerC = new Stopwatch();
                 Arrays.sort(c);
+                timeC = timerC.elapsedTimeMillis();
+
+                //sorting array d w/ time using the default
+                timerD = new Stopwatch();
                 Arrays.sort(d);
+                timeD = timerD.elapsedTimeMillis();
                 break;
         }
-         // TODO: For each array, a, b, c, d:  
-        Stopwatch timer = new Stopwatch();
-        // TODO: Perform Sorting and store the result in an  array
 
-        double time = timer.elapsedTimeMillis();
-        
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
           //TODO: Replace with your own netid
-        System.out.println(timeStamp);
-        String netID = "tbiswas2";
+        String netID = "32362206";
           //TODO: Replace with the algorithm used 
-        String algorithmUsed = "insertion sort";
-          //TODO: Replace with the  array used 
-        String arrayUsed = "a";
-         // StdOut.printf("%s %s %8.1f   %s  %s  %s\n", algorithmUsed, arrayUsed, time, timeStamp, netID, args[0]);
-          // Write the resultant array to a file (Each time you run a program 4 output files should be generated. (one for each a,b,c, and d)
+        String algorithmUsed = name + " sort";
+        //TODO: Replace with the array used
+        StdOut.printf("%s %s %8.1f   %s  %s  %s\n", algorithmUsed, "a", timeA, timeStamp, netID, args[2]);
+        StdOut.printf("%s %s %8.1f   %s  %s  %s\n", algorithmUsed, "b", timeB, timeStamp, netID, args[2]);
+        StdOut.printf("%s %s %8.1f   %s  %s  %s\n", algorithmUsed, "c", timeC, timeStamp, netID, args[2]);
+        StdOut.printf("%s %s %8.1f   %s  %s  %s\n", algorithmUsed, "d", timeD, timeStamp, netID, args[2]);
+        // Write the resultant array to a file (Each time you run a program 4 output files should be generated. (one for each a,b,c, and d)
   }
 
 
